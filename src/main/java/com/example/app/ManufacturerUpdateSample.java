acd2047b8b71a11c4792be47303d70a036cc210a
Tada <Tada@Tada-THINK>
2015-03-24 15:43:35 +0900

    create 1 sample class.

---
+++ src/main/java/com/example/app/ManufacturerUpdateSample.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.app;
+
+import com.example.entity.Manufacturer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Tada
+ */
+public class ManufacturerUpdateSample {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("samplePU");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        
+        Manufacturer manufacturer = manager.find(Manufacturer.class, 1);
+        manufacturer.setZip("140-0001");
+        
+        manager.flush();
+        tx.commit();
+        
+        manager.close();
+        factory.close();
+    }
+}
